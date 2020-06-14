ed31833ce974023b3a7d2840bffc83f7a543808b
Adashuai5 <ad873606339@gmail.com>
2020-06-14 05:37:48 +0000

    Deploying to gh-pages from master 8ed4845fc99ee673bf116bd110db38ef52469dd4

---
+++ precache-manifest.dd94f0757967e6b692ee3e3956c4b8c2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7087043048e9f02b1a7dc9caab6b8e79",
+    "url": "/my-desktop/index.html"
+  },
+  {
+    "revision": "d414777fc4b1bf341b63",
+    "url": "/my-desktop/static/css/main.614dfb4d.chunk.css"
+  },
+  {
+    "revision": "bd06c2a4d91cc4eb9a09",
+    "url": "/my-desktop/static/js/2.99985698.chunk.js"
+  },
+  {
+    "revision": "846e3710ca5ee3f911296f05447fe08c",
+    "url": "/my-desktop/static/js/2.99985698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d414777fc4b1bf341b63",
+    "url": "/my-desktop/static/js/main.275597b6.chunk.js"
+  },
+  {
+    "revision": "f9b42350e7efbb4fee1c",
+    "url": "/my-desktop/static/js/runtime-main.ac3fd019.js"
+  },
+  {
+    "revision": "5412703c080e52febe4dc46e0a45281c",
+    "url": "/my-desktop/static/media/Calculator.5412703c.png"
+  },
+  {
+    "revision": "b234fa5e5aa41eecfde4537d89b0804b",
+    "url": "/my-desktop/static/media/Chrome.b234fa5e.png"
+  },
+  {
+    "revision": "0def2931b880e4af977622adcd95d527",
+    "url": "/my-desktop/static/media/Drawing.0def2931.png"
+  },
+  {
+    "revision": "35af0dc523243328ba114cd64797d590",
+    "url": "/my-desktop/static/media/Finder.35af0dc5.png"
+  },
+  {
+    "revision": "b6471dc48498ceeafa60e85a95c20dff",
+    "url": "/my-desktop/static/media/Launchpad.b6471dc4.png"
+  },
+  {
+    "revision": "b6a1faceb0d91099175879f656df1da9",
+    "url": "/my-desktop/static/media/PrefApp.b6a1face.png"
+  },
+  {
+    "revision": "bc2c9be74512cca94ef6eb0293c457fe",
+    "url": "/my-desktop/static/media/Terminal.bc2c9be7.png"
+  },
+  {
+    "revision": "3890707cf58c2e6b1bd5ead0380c541b",
+    "url": "/my-desktop/static/media/sierra-4K.3890707c.jpg"
+  }
+]);
